43e8d00fdc60e46596aa8edab35fa1db710e72d5
Zikrulloh01 <96194416+Zikrulloh01@users.noreply.github.com>
2021-12-23 20:40:24 +0000

    Change from github.com

--- src/test/java/TC_001.java
+++ src/test/java/TC_001.java
@@ -2,7 +2,8 @@ public class TC_001 {
     public static void main(String[] args) {
 
         System.out.println("Change after cloning locally");
-
+        System.out.println("Change from github.com");
+        
 
     }
 }
